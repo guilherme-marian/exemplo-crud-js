450d788854a5c133234bc20db902b243edf9950f
guilherme-marian <guimarian123@gmail.com>
2025-07-03 15:28:51 -0300

    Aula 2

--- index.js
+++ index.js
@@ -4,11 +4,24 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 const connection = mysql.createConnection ({
     host: 'localhost',
     user: 'root',
     password: 'root',
     database: 'controleEstoque'
+});
+
+connection.connect (function(err) {
+    if(err) {
+        console.error("Error: ", err);
+        return;
+    }
+    else {
+        console.log("Ok Connection");
+    }
 });
 
 app.get('/', (req, res) => {
@@ -19,6 +32,25 @@ app.get('/cadastro', (req, res) => {
     res.sendFile(__dirname + '/cadastro.html');
 });
 
+app.post('/cadastrar', (req, res) => {
+    const produto = req.body.nome;
+    const quantidade = req.body.quantidade;
+    const preco = req.body.preco;
+
+    const insert = 
+        'INSERT INTO produtos (produto, quantidade, preco) VALUES (?, ?, ?)';
+    connection.query(insert, [produto, quantidade, preco], (err, results) => {
+        if(err){
+            console.error("Error to insert product: ", err);
+            res.status(500).send("Error to register the product");
+            return;
+        }
+        else {
+            console.log("Product was insert with success!");
+        }
+    })
+});
+
 app.listen(3000, () => {
-    console.log('Servidor rodando em http://localhost:3000');
+    console.log('Server running http://localhost:3000');
 });
