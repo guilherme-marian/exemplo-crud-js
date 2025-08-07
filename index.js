049e83c2ac3a63a2cb3e9f41ba7bcdcdb33df0a6
guilherme-marian <guimarian123@gmail.com>
2025-08-07 13:59:32 -0300

    atualizar

--- index.js
+++ index.js
@@ -105,6 +105,50 @@ app.get('/deletar/:id', (req, res) => {
     });
 });
 
+app.get('/atualizar/:id', (req, res) => {
+    const id = req.params.id;
+    const select = 'SELECT * FROM produtor WHERE id = ?';
+
+    connection.query(select, [id], (err, rows) => {
+        if (!err && rows.length > 0)
+        {
+            const produto = rows[0];
+            res.send(
+                `
+                <html>
+                    <head>
+                        <title>Atualizar Produto</title>
+                    </head>
+                    <body>
+                        <h1>Atualizar Produto</h1>
+                        <form action="/atualizar/${produto.id}" method="POST">
+                            <label for="nome">Nome:</label>
+                            <input type="text" id="nome" name="nome=" 
+                            value ="${produto.produto}" required><br><br>
+
+                            <label for="quantidade">Quantidade:</label>
+                            <input type="number" id="quantidade" name="quantidade"
+                            value="${produto.quantidade}" required><br><br>
+
+                            <label for="preco">Preço:</label>
+                            <input type="number" step="0.01" id="preco" name="preco"
+                            value="${produto.preco}" required><br><br>
+
+                            <input type="submite" value="Atualizar">
+                        </form>
+
+                        <a href="/relatorio">Voltar</a>
+                    </body>
+                </html>
+                `
+            );
+        } 
+        else {
+            res.status(404).send("Produto não encontrado");
+        }
+    });
+});
+
 app.listen(3000, () => {
     console.log('Server running http://localhost:3000');
 });
